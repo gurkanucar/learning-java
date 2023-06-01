1031ffab8bf4468bac7f23f8bad6329eeda5573a
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-06-01 14:00:57 +0300

    feat: OrderBuilder client example created in main method

--- src/main/java/org/gucardev/fluentAndStepPattern/Main.java
+++ src/main/java/org/gucardev/fluentAndStepPattern/Main.java
@@ -3,6 +3,17 @@ package org.gucardev.fluentAndStepPattern;
 public class Main {
 
   public static void main(String[] args) {
-    //
+    Order order =
+        OrderBuilder.builder()
+            .setAccount("email@example.com")
+            .setShippingAddress("123 Street, City, State, Country")
+            .setPaymentMethod("Credit Card")
+            .addProduct("Book", 15.99, 1)
+            .addProduct("Laptop", 1200.00, 1)
+            .finishProducts()
+            .setDeliveryOption("Express")
+            .build();
+
+    System.out.println(order);
   }
 }
