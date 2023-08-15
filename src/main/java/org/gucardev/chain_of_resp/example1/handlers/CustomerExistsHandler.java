5cc0db99fa5efd73a2e1546cb6710f10a44be70f
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-08-15 13:19:45 +0300

    feat: chain of responsibility design pattern example created

---
+++ src/main/java/org/gucardev/chain_of_resp/example1/handlers/CustomerExistsHandler.java
@@ -0,0 +1,18 @@
+package org.gucardev.chain_of_resp.example1.handlers;
+
+import org.gucardev.chain_of_resp.example1.Handler;
+import org.gucardev.chain_of_resp.example1.User;
+
+public class CustomerExistsHandler extends Handler {
+
+  @Override
+  public boolean handle(Object payload) {
+    User user = (User) payload;
+    if (user.getId() == null) {
+      System.out.println("customer does not exist!");
+      return false;
+    }
+    System.out.println("CustomerExistsHandler success");
+    return handleNext(payload);
+  }
+}
